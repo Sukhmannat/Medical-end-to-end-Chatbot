fadad7945a63e0a14af10d1fb2f04731d0aaf822
Sukhmannat Kaur <mannatshabad@gmail.com>
2025-08-28 20:18:15 +0530

    Added full project files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Sukhmannat Kaur',
+    author_email= 'mannatshabad@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
